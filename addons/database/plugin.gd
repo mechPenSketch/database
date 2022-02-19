ec6975ec8b9ed6f9bb8642624294439c02b629ae
mechPenSketch <pikadon92@gmail.com>
2022-02-19 17:41:05 +0800

    Refactoring

--- addons/database/plugin.gd
+++ addons/database/plugin.gd
@@ -5,6 +5,8 @@ const MainPanel = preload("main_screen/MainScreen.tscn")
 var main_panel_instance
 const Tab = preload("main_screen/Tab.tscn")
 
+const DATA_DIR = "res://data"
+
 func _enter_tree():
 	main_panel_instance = MainPanel.instance()
 	get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
@@ -14,13 +16,15 @@ func _enter_tree():
 	
 	#	DEFINE NEW TAB
 	var tab_container = main_panel_instance.get_node("TabContainer")
-	
+	#print (tab_container.get_current_tab_control())
 	
 	var dir = Directory.new()
-	match dir.open("data"):
+	var open_error = dir.open(DATA_DIR)
+	
+	match open_error:
 		OK:
 			# FOR EVERY ITEM IN /content
-			dir.list_dir_begin()
+			dir.list_dir_begin(true)
 			var file_name = dir.get_next()
 			while file_name != "":
 				# IF THE ITEM IS A FOLDER
@@ -29,12 +33,13 @@ func _enter_tree():
 					var tab_instance = Tab.instance()
 					tab_instance.set_name(file_name.capitalize())
 					main_panel_instance.add_child(tab_instance)
-				file_name.go_next()
-		ERR_FILE_NOT_FOUND:
+				file_name = dir.get_next()
+			dir.list_dir_end()
+		ERR_INVALID_PARAMETER:
 			# NEW FOLDER
 			dir.make_dir("data")
 		_:
-			print("Error")
+			print(open_error)
 
 func _exit_tree():
 	if main_panel_instance:
