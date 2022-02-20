4d480eb42dd2e4adca63ec4f3d0616c8d5321bdc
mechPenSketch <pikadon92@gmail.com>
2022-02-20 20:10:59 +0800

    Added signal for FileSystem Changing

--- addons/database/plugin.gd
+++ addons/database/plugin.gd
@@ -25,25 +25,54 @@ func _enter_tree():
 		OK:
 			# FOR EVERY ITEM IN /content
 			dir.list_dir_begin(true)
-			var file_name = dir.get_next()
-			while file_name != "":
+			var folder_name = dir.get_next()
+			while folder_name != "":
+				print(folder_name)
+					
 				# IF THE ITEM IS A FOLDER
 				if dir.current_is_dir():
-					# DD NEW TAB TO CONTAINER
+					
+					# ADD NEW TAB TO CONTAINER
 					var tab_instance = Tab.instance()
-					tab_instance.set_name(file_name.capitalize())
+					tab_instance.set_name(folder_name.capitalize())
 					main_panel_instance.add_child(tab_instance)
-				file_name = dir.get_next()
+					
+					# ADD LIST OF FILES INTO LINEEDIT
+					var sub_dir = Directory.new()
+					#	SUB OPEN ERROR
+					var sub_oe = sub_dir.open(DATA_DIR +"/"+ folder_name)
+					
+					match sub_oe:
+						OK:
+							var le_txt = ""
+							sub_dir.list_dir_begin(true)
+							var file_name = sub_dir.get_next()
+							while file_name != "":
+								#print(file_name)
+								le_txt += file_name
+								file_name = sub_dir.get_next()
+							sub_dir.list_dir_end()
+						_:
+							print(sub_oe)
+					
+				folder_name = dir.get_next()
 			dir.list_dir_end()
 		ERR_INVALID_PARAMETER:
 			# NEW FOLDER
 			dir.make_dir("data")
 		_:
 			print(open_error)
+			
+	# FILE SYSTEM
+	var filesystem = get_editor_interface().get_resource_filesystem()
+	filesystem.connect("filesystem_changed", self, "_on_filesystem_changed")
 
 func _exit_tree():
 	if main_panel_instance:
 		main_panel_instance.queue_free()
+
+func _on_filesystem_changed():
+	pass
 
 func has_main_screen():
 	return true
